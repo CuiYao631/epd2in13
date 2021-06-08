083ffa9f12ee67f2d5341586df5bca2ab437d7f1
CuiYao631 <631836754@qq.com>
2021-06-08 09:28:32 +0800

    添加了wifi配网功能~待测试

--- espwifi.h
+++ espwifi.h
@@ -1,15 +1,23 @@
 
-
+//配网模式下需要连接的WIFI
+#ifndef STASSID
+#define STASSID "XiaoCuiKeJi"
+#define STAPSK "Cui123456"
+#endif
 class espwifi
 {
 private:
-    /* data */
+   
 public:
+   
     espwifi(/* args */);
     ~espwifi();
     void Init(void);
-    bool ConnectWifi(void);
+    bool HtmlConfig(void);
     bool AutoConfigWifi(void);
     void BreakOffWifi(void);
     void Recovery(void);
+    static void HandleRoot(void);
+    static void HandleVal(void);
+    static void HandleNotFound(void);
 };
